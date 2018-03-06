2e66c5252a0ea3b9bfb20c622465fec44127deea
Simeon Vincent <svincent@gmail.com>
2018-03-06 10:38:13 -0800

    Refactor to facilitate type flow

--- models/observable.ts
+++ models/observable.ts
@@ -3,24 +3,25 @@ import { Subscription } from './subscription';
 import { SubscriberFunction } from './subscription-function';
 import { Observer } from './observer';
 
+type ObservableObserver<T, E> = (observer: Observer<T, E>) => Subscription<T, E>;
+type ObservableObject<T, E> = (onNext: Function,
+                              onError?: (E) => void,
+                              onComplete?: Function) => Subscription<T, E>;
+
 export interface Observable<T, E = any> {
 
-  constructor(subscriber: SubscriberFunction);
+  constructor(subscriber: SubscriberFunction<T, E>);
 
-  /** Subscribes to the sequence with an observer */
-  subscribe(observer: Observer<T, E>): Subscription<T, E>;
-
-  /** Subscribes to the sequence with callbacks */
-  subscribe(onNext: Function,
-            onError?: (E) => void,
-            onComplete?: Function): Subscription<T, E>;
+  /** Subscribes to the sequence with an observer or with callbacks*/
+  subscribe?: ObservableObserver<T, E> | ObservableObserver<T, E>;
 
   /** Returns itself */
-  [SymbolObservable](): Observable<T>;
+  [SymbolObservable](): Observable<T, E>;
 
   /** Converts items to an Observable */
-  of<T>(items: T[]): Observable<T>;
+  of<T>(items: T[]): Observable<T, E>;
 
   /** Converts an observable or iterable to an Observable */
-  from(observable): Observable<T>;
+  from(observable): Observable<T, E>;
+
 }
