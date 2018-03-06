d91df4bc2115aeee962d1cb10883e559bc74f54a
Simeon Vincent <svincent@gmail.com>
2018-03-06 10:38:33 -0800

    Refactor to facilitate type flow

--- models/observer.ts
+++ models/observer.ts
@@ -2,14 +2,15 @@ import { Subscription } from "./subscription";
 
 export interface Observer<T, E = any> {
   /** Receives the subscription object when `subscribe` is called */
-  start?(subscription: Subscription<T, E>);
+  start?: (subscription: Subscription<T, E>) => void;
 
   /** Receives the next value in the sequence */
-  next?(value: T);
+  next?: (value: T) => void;
 
   /** Receives the sequence error */
-  error?(errorValue: E);
+  error?: (errorValue: E) => void;
 
   /** Receives a completion notification */
-  complete?();
+  complete?: () => void;
 }
+                          
